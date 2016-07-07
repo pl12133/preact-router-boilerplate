a10d2b4f7317c9a3ddf7cbd452ddab169569269d
pl12133 <krirken@gmail.com>
2016-07-07 15:45:00 -0400

    :shipit: Publish gh-pages demo

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar/';
-export { default as WelcomeCaption } from './WelcomeCaption/';
-export { default as Footer } from './Footer/';
-export { default as LoremIpsum } from './LoremIpsum/';
-export { default as FontAwesome } from './FontAwesome/';
